7df865d3a9c170d6fd29ec794d764602db5d8661
CindoddCindy <cindodcindy@gmail.com>
2021-03-17 16:54:06 +0700

    tambah kelas kelas

---
+++ futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/BottomNav.java
@@ -0,0 +1,16 @@
+package com.futrashproject.futrashmitra.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.futrashproject.futrashmitra.R;
+
+public class BottomNav extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bottom_nav);
+    }
+}
---
+++ futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/DetailItem.java
@@ -0,0 +1,16 @@
+package com.futrashproject.futrashmitra.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.futrashproject.futrashmitra.R;
+
+public class DetailItem extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detail_item);
+    }
+}
