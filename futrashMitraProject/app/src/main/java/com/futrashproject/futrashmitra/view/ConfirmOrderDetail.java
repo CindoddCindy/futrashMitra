6993794e35d9b702e5896cdb3cce910f06db982e
CindoddCindy <cindodcindy@gmail.com>
2021-04-06 19:19:34 +0700

    tambah view detail confirm detail

--- futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/ConfirmOrderDetail.java
+++ futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/ConfirmOrderDetail.java
@@ -8,8 +8,8 @@ import android.widget.TextView;
 import com.futrashproject.futrashmitra.R;
 
 public class ConfirmOrderDetail extends AppCompatActivity {
-    private TextView textView_nama_penjual, textView_nama_makanan, textView_lokasi_customer, textView_nama_customer,
-    textView_phone_customer, textView_konfirm_terima_tidak, textView_catata_alasan, textView_edit, textView_hapus, textView_back;
+    private TextView textView_nama_penjual, textView_nama_makanan,textView_date_item, textView_lokasi_customer, textView_nama_customer,
+    textView_phone_customer, textView_shipping_type,textView_tanggal_konfirmasi_order, textView_konfirm_terima_tidak, textView_catata_alasan, textView_edit, textView_hapus, textView_back;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,9 +18,12 @@ public class ConfirmOrderDetail extends AppCompatActivity {
 
         textView_nama_penjual=findViewById(R.id.tv_confirm_detail_nama_penjual);
         textView_nama_makanan=findViewById(R.id.tv_confirm_detail_jenis_makanan);
+        textView_date_item=findViewById(R.id.tv_confirm_detail_date_item);
         textView_lokasi_customer=findViewById(R.id.tv_confirm_detail_location_customer);
         textView_nama_customer=findViewById(R.id.tv_confirm_detail_cutomer_name);
         textView_phone_customer=findViewById(R.id.tv_confirm_detail_cust_phone);
+        textView_shipping_type=findViewById(R.id.tv_confirm_detail_shiping_type);
+        textView_tanggal_konfirmasi_order=findViewById(R.id.tv_confirm_item_tanggal_confirm);
         textView_konfirm_terima_tidak=findViewById(R.id.tv_confirm_detail_terima_tolak);
         textView_catata_alasan=findViewById(R.id.tv_confirm_detail_catatan_alasan);
         textView_edit=findViewById(R.id.tv_confirm_detail_btn_Edit);
