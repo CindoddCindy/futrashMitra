bf4e139274833ede4351fd0ba4879e30c5c25afc
CindoddCindy <cindodcindy@gmail.com>
2021-04-07 17:15:05 +0700

    tambah rv confirm

--- futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/servis/MethodsFactory.java
+++ futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/servis/MethodsFactory.java
@@ -1,5 +1,6 @@
 package com.futrashproject.futrashmitra.servis;
 
+import com.futrashproject.futrashmitra.model.pojo_confirm.confirm_get.FoodTrashGetConfirmMitraRespon;
 import com.futrashproject.futrashmitra.model.pojo_confirm.post_confirm.FoodTrashPostConfirmMitraRespon;
 import com.futrashproject.futrashmitra.model.pojo_item.pojo_get_item.FoodTrashMitraGetItemRespon;
 import com.futrashproject.futrashmitra.model.pojo_item.pojo_post_item.FoodTrashMitraPostItemRespon;
@@ -61,5 +62,14 @@ public interface MethodsFactory {
     })
     @POST("confirm/users/{id}/confirm")
     Call<FoodTrashPostConfirmMitraRespon> postConfirm(@Path ("id") Long id, @HeaderMap Map<String,String > Map, @Body JsonObject body);
+
+
+
+    @Headers({
+            "Content-Type:application/json"
+    })
+    @GET("confirm/users/{id}/confirm")
+    Call<FoodTrashGetConfirmMitraRespon> getListConfirm(@Path ("id") Long id, @HeaderMap Map<String,String > Map);
+
 
 }
--- futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/fragment/FragmentConfirm.java
+++ futrashMitraProject/app/src/main/java/com/futrashproject/futrashmitra/view/fragment/FragmentConfirm.java
@@ -3,12 +3,33 @@ package com.futrashproject.futrashmitra.view.fragment;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import com.futrashproject.futrashmitra.R;
+import com.futrashproject.futrashmitra.model.pojo_confirm.confirm_get.Content;
+import com.futrashproject.futrashmitra.model.pojo_confirm.confirm_get.FoodTrashGetConfirmMitraRespon;
+import com.futrashproject.futrashmitra.model.pojo_item.pojo_get_item.FoodTrashMitraGetItemRespon;
+import com.futrashproject.futrashmitra.servis.MethodsFactory;
+import com.futrashproject.futrashmitra.servis.RetrofitHandle;
+import com.futrashproject.futrashmitra.shared_preference.SpHandle;
+import com.futrashproject.futrashmitra.view.adapter.ConfirmAdapter;
+import com.futrashproject.futrashmitra.view.adapter.ItemAdapter;
+import com.futrashproject.futrashmitra.view.adapter.OrderAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -25,6 +46,13 @@ public class FragmentConfirm extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    private RecyclerView recyclerView;
+    private ConfirmAdapter confirmAdapter;
+    private List<Content> contentList = new ArrayList<>();
+    private MethodsFactory methodsFactory;
+    private SpHandle spHandle;
+
 
     public FragmentConfirm() {
         // Required empty public constructor
@@ -61,6 +89,71 @@ public class FragmentConfirm extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm, container, false);
+        View view = inflater.inflate(R.layout.fragment_confirm, container, false);
+
+        spHandle = new SpHandle(getContext());
+
+        recyclerView = view.findViewById(R.id.rv_confirm_list_item);
+        confirmAdapter = new ConfirmAdapter( contentList,getContext());
+        recyclerView.setAdapter(confirmAdapter);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+
+        return view;
+    }
+
+
+    public void getConfirmList(){
+        Long id = spHandle.getSpIdUser();
+
+        String tokenUser = spHandle.getSpTokenUser();
+        Map<String,String> token = new HashMap<>();
+        token.put("Authorization", "Bearer "+tokenUser);
+
+
+        methodsFactory = RetrofitHandle.getRetrofitLink().create(MethodsFactory.class);
+        Call<FoodTrashGetConfirmMitraRespon> orderListCall=methodsFactory.getListConfirm(id, token);
+        orderListCall.enqueue(new Callback<FoodTrashGetConfirmMitraRespon>() {
+            @Override
+            public void onResponse(Call<FoodTrashGetConfirmMitraRespon> call, Response<FoodTrashGetConfirmMitraRespon> response) {
+
+                if (response.isSuccessful()) {
+                    // response.body().getData();
+                    List<Content> content = response.body().getContent();
+                    confirmAdapter = new ConfirmAdapter(content,getContext());
+                    recyclerView.setAdapter(confirmAdapter);
+                    //adapterIndonesia = new AdapterIndonesia(getContext(),propinsiAtributes);
+                    //recyclerView.setAdapter(adapterIndonesia);
+                    confirmAdapter.notifyDataSetChanged();
+                }
+                else {
+                    // error case
+                    switch (response.code()) {
+                        case 404:
+                            Toast.makeText(getContext(), "404 not found", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 500:
+                            Toast.makeText(getContext(), "500 internal server error", Toast.LENGTH_SHORT).show();
+                            break;
+                        case 401:
+                            Toast.makeText(getContext(), "401 unauthorized", Toast.LENGTH_SHORT).show();
+                            break;
+
+                        default:
+                            Toast.makeText(getContext(), "unknown error", Toast.LENGTH_SHORT).show();
+                            break;
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call<FoodTrashGetConfirmMitraRespon> call, Throwable t) {
+                Toast.makeText(getContext(), "network failure :( inform the user and possibly retry ", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+
     }
 }
